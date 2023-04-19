38504ee9c723d1de5fe8cc8d6975b8316e6fbbad
Radhey Parekh <eshkon.radhey@gmail.com>
2023-04-19 20:10:49 +0530

    +More features

--- user-search/src/components/search/index.jsx
+++ user-search/src/components/search/index.jsx
@@ -1,8 +1,18 @@
 import { useState, useEffect } from "react";
 
-function UserCard({ user }) {
+function UserCard({  user, handleKeyDown, handleMouseEnter, handleMouseLeave, focused, highlightedUser, highlightMatch } ) {
+
+  const isHighlighted = user.id === highlightedUser;
+  const name = highlightMatch(user.name);
+  const address = highlightMatch(user.address);
+  
   return (
-    <div className="card">
+    <div className={`card${focused ? ' focused' : ''} card${highlightedUser && highlightedUser.id === user.id ? "highlighted" : null}`} 
+    tabIndex="0"
+    onKeyDown={(e) => handleKeyDown(e, user)}
+    onMouseEnter={() => handleMouseEnter(user)}
+    onMouseLeave={() => handleMouseLeave(user)}
+  >
       <div>ID: {user.id}</div>
       <div>Name: {user.name}</div>
       <div>Items: {user.items.join(', ')}</div>
@@ -17,6 +27,11 @@ export default function SearchUsers() {
   const [filteredUsers, setFilteredUsers] = useState([]);
   const [searchQuery, setSearchQuery] = useState('');
   const [showResults, setShowResults] = useState(false);
+  const [focusedUser, setFocusedUser] = useState(null);
+  const [highlightedUser, setHighlightedUser] = useState(null);
+  const [hoveredUser, setHoveredUser] = useState(null);
+  const [isMouseActive, setIsMouseActive] = useState(false);
+  const [isUsingKeyboard, setIsUsingKeyboard] = useState(false)
 
   useEffect(() => {
     fetch('/api/search')
@@ -50,8 +65,69 @@ export default function SearchUsers() {
     setShowResults(true);
   }, [searchQuery, users]);
 
-  function handleSearchChange(event) {
+  function highlightMatch(text) {
+    if (!searchQuery) {
+      return text;
+    }
+  
+    const regex = new RegExp(searchQuery, "gi");
+    const matches = text.match(regex);
+  
+    if (!matches) {
+      return text;
+    }
+  
+    return text.split(regex).map((part, index) => {
+      if (index === 0) {
+        return part;
+      }
+      return (
+        <span key={index} className="highlight">
+          {matches[index - 1]}
+        </span>
+      );
+    });
+  }
+
+  const handleSearchChange = (event) =>  {
     setSearchQuery(event.target.value);
+  }
+
+  function handleKeyDown(event, user) {
+    setIsUsingKeyboard(true);
+    if (event.key === 'Enter') {
+      console.log('User selected:', user);
+    } else if (event.key === 'ArrowDown') {
+      const index = users.findIndex((u) => u.id === user.id);
+      setFocusedUser(users[index + 1] || users[0]);
+      setIsMouseActive(false);
+    } else if (event.key === 'ArrowUp') {
+      const index = users.findIndex((u) => u.id === user.id);
+      setFocusedUser(users[index - 1] || users[users.length - 1]);
+      setIsMouseActive(false);
+    }
+  }
+  
+
+  const handleMouseEnter = (user) => {
+    setIsUsingKeyboard(false);
+    if(!isMouseActive) {
+      setFocusedUser(user);
+    }
+    if(!isUsingKeyboard){
+      setHighlightedUser(user.id)
+    }
+    setHoveredUser(user);
+  }
+
+  const handleMouseLeave = (user) => {
+    if(!isMouseActive) {
+      setFocusedUser(null);
+    }
+    if(!isUsingKeyboard){
+      setHighlightedUser(null)
+    }
+    setHoveredUser(null);
   }
 
   return (
@@ -65,7 +141,13 @@ export default function SearchUsers() {
       {showResults ? (
         <div className="card-container">
           {filteredUsers.map((user) => (
-            <UserCard key={user.id} user={user} />
+            <UserCard key={user.id} user={user} 
+            handleKeyDown={handleKeyDown} 
+            handleMouseEnter={handleMouseEnter} 
+            handleMouseLeave={handleMouseLeave} 
+            focused={isMouseActive ? hoveredUser?.id === user.id : focusedUser?.id === user.id} 
+            highlightedUser={highlightedUser}
+            highlightMatch={highlightMatch}/>
           ))}
         </div>
       ) : <div className="card-container">
